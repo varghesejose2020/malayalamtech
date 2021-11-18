0baa701be526756846ffd97fa1cc8770aabc0b47
Varghese Jose <71702982+varghesejose2020@users.noreply.github.com>
2021-11-18 18:21:20 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-// const { Step } = Steps;
 
-
-
-
-// ReactDOM.render(
-//   <Steps current={1}>
-//     <Step title="Finished" description="This is a description." />
-//     <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
-//     <Step title="Waiting" description="This is a description." />
-//   </Steps>,
-//   mountNode,
-// );
 
 const IndexPage = () => (
   <Layout>
@@ -36,12 +24,12 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
-//     <p>
-//       <Link to="/page-2/">Go to page 2</Link> <br />
-//       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-//       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-//       <Link to="/using-dsg">Go to "Using DSG"</Link>
-//     </p>
+<p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
+    </p>
   </Layout>
 )
 
